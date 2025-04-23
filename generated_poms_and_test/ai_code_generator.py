197fde4b9821708c402b03c8d4566c79fcd7823b
csabida01 <csabida01@gmail.com>
2025-04-23 22:16:36 +0200

    update generator file

--- generated_poms_and_test/ai_code_generator.py
+++ generated_poms_and_test/ai_code_generator.py
@@ -464,7 +464,7 @@ while True:
         raise Exception("Assistant run failed.")
     time.sleep(1)
 
-time.sleep(15)
+time.sleep(18)
 messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
 
 message_content = messages[0].content[0].text.value
